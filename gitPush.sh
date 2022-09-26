a0b2034153b491cfbf8e1c89e3519bf81938e194
John Wati <john.wati.ke@gmail.com>
2022-09-26 20:45:02 +0300

    updating git bash file

--- gitPush.sh
+++ gitPush.sh
@@ -1,9 +1,32 @@
-echo "=============Adding Files to Commit========="
-git add .
-echo "==========Enter Commit Message =============="  
-read commit_message  
-git commit -m $commit_message
-echo "=============Commit Files to Repo========="
- git push --set-upstream origin master
-git push
+# echo "=============Adding Files to Commit========="
+# git add .
+# echo "==========Enter Commit Message =============="  
+# read commit_message  
+# git commit -m $commit_message
+# echo "=============Commit Files to Repo========="
+#  git push --set-upstream origin master
+# git push
 
+# Display unstaged files
+ echo "==========Display unstaged files============"
+git status
+
+if [ "$(git status --porcelain)" ]; then
+        echo "There are uncommited and unstatged files. Commit them before pushing"
+        echo "==========Enter commit message============"
+        read commitMessage
+        echo "=============Adding Files to Commit========="
+        git add .
+        git commit -m "$commitMessage"
+        git push
+        echo "****************Files pushed to GitHub****************"
+        # else push all commited and staged files to remote repo
+    else
+        git push
+        echo "***********Files pushed to GitHub*****************"
+        
+    fi
+    #Echo message if there is no files to commit, stage or push
+    if [ "$(git status --porcelain)" ]; then
+        echo "There are no files to commit, stage or push"
+    fi
